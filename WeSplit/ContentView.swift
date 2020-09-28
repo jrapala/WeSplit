13c75461a338deae59a787c84938f3fb27473202
Juliette Rapala <jrapala@gmail.com>
2020-09-27 19:35:15 -0500

    fix: change modifier logic

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -60,7 +60,7 @@ struct ContentView: View {
                 
                 Section(header: Text("Total amount with tip")) {
                     Text("$\(totalCheckAmount, specifier: "%.2f")")
-                        .foregroundColor(tipPercentage == 4 ? .red : .black)
+                        .foregroundColor(tipPercentages[tipPercentage] == 0 ? .red : .black)
                 }
                 
                 Section(header: Text("Amount per person")) {
