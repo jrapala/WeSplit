ab45529c15b2ca487590f5b4ade6f71e97887b9a
Juliette Rapala <jrapala@gmail.com>
2020-09-27 19:34:05 -0500

    feat: use red text if 0 percent tip is selected

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -60,6 +60,7 @@ struct ContentView: View {
                 
                 Section(header: Text("Total amount with tip")) {
                     Text("$\(totalCheckAmount, specifier: "%.2f")")
+                        .foregroundColor(tipPercentage == 4 ? .red : .black)
                 }
                 
                 Section(header: Text("Amount per person")) {
